bc83a9d4d0fbb3fb430ab02572dd99daae3f4353
Sébastien NEDJAR <nedseb@goldaire.com>
2014-10-16 10:34:06 +0200

    ajout du code Java

---
+++ testJDBC.java
@@ -0,0 +1,47 @@
+// Ne pas faire un copier/coller du pdf...
+
+// Importer les classes jdbc
+import java.sql.*;
+
+public class testJDBC {
+	// Chaine de connexion
+	static final String CONNECT_URL = "jdbc:mysql://localhost:3306/maBD";
+	static final String LOGIN = "monLogin";
+	static final String PASSWORD = "monPaswd";
+	// La requete de test
+	static final String req = "SELECT NUM_ET, NOM_ET, PRENOM_ET " +
+	                          "FROM ETUDIANT " +
+	                          "WHERE VILLE_ET = 'AIX-EN-PROVENCE'";                                     
+	public static void main(String[] args) throws SQLException {
+		// Objet materialisant la connexion a la base de donnees
+		Connection conn = null;
+		try {
+			// Connexion a la base
+			System.out.println("Connexion a " + connectString );
+			conn = DriverManager.getConnection(CONNECT_URL,LOGIN,PASSWORD);
+			System.out.println("Connecte\n");
+			// Creation d'une instruction SQL
+			Statement stmt = conn.createStatement();
+			// Execution de la requete
+			System.out.println("Execution de la requete : " + req );
+			ResultSet rset = stmt.executeQuery(req);
+			// Affichage du resultat
+			while (rset.next()){	
+				System.out.print(rset.getInt("NUM_ET") + " ");
+				System.out.print(rset.getString("NOM_ET") + " ");
+				System.out.println(rset.getString("PRENOM_ET"));
+			}
+			// Fermeture de l'instruction (liberation des ressources)
+			stmt.close();
+			System.out.println("\nOk.\n");
+		} catch (SQLException e) {
+			e.printStackTrace();// Arggg!!!
+			System.out.println(e.getMessage() + "\n");
+		} finally {
+			if (conn != null) {
+				// Deconnexion de la base de donnees
+				conn.close();
+			}
+		}
+	}
+}
