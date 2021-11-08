995b1bd77ad67ce733746c0696bd33e76f818689
nedseb <sebastien.nedjar@univ-amu.fr>
2021-11-08 12:47:24 +0000

    install sonarCloud

--- src/main/java/fr/univ_amu/iut/TestJDBC.java
+++ src/main/java/fr/univ_amu/iut/TestJDBC.java
@@ -2,6 +2,8 @@ package fr.univ_amu.iut;
 
 // Importer les classes jdbc
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class TestJDBC {
 	// Chaine de connexion
@@ -9,32 +11,29 @@ public class TestJDBC {
 	static final String LOGIN = "monUser";
 	static final String PASSWORD = "monPassword";
 	// La requete de test
-	static final String req = "SELECT NUM_ET, NOM_ET, PRENOM_ET " +
-	                          "FROM ETUDIANT " +
-	                          "WHERE VILLE_ET = 'AIX-EN-PROVENCE'";
+	static final String QUERY = "SELECT NUM_ET, NOM_ET, PRENOM_ET " + "FROM ETUDIANT "
+			+ "WHERE VILLE_ET = 'AIX-EN-PROVENCE'";
+
+	private static final Logger logger = Logger.getLogger("TestJDBC");
 
 	public static void main(String[] args) throws SQLException {
 		// Connexion a la base de données
-		System.out.println("Connexion a " + CONNECT_URL);
-		try (Connection conn = DriverManager.getConnection(CONNECT_URL,LOGIN,PASSWORD)){
-			System.out.println("Connecte\n");
-			// Creation d'une instruction SQL
-			Statement stmt = conn.createStatement();
-			// Execution de la requete
-			System.out.println("Execution de la requete : " + req );
-			ResultSet rset = stmt.executeQuery(req);
-			// Affichage du resultat
-			while (rset.next()){	
-				System.out.print(rset.getInt("NUM_ET") + " ");
-				System.out.print(rset.getString("NOM_ET") + " ");
-				System.out.println(rset.getString("PRENOM_ET"));
+		logger.log(Level.INFO, "Connexion a " + CONNECT_URL);
+		try (Connection connection = DriverManager.getConnection(CONNECT_URL, LOGIN, PASSWORD)) {
+			logger.log(Level.INFO, "Création instruction SQL");
+			try (Statement statement = connection.createStatement()) {
+				logger.log(Level.INFO, "Execution de la requete : " + QUERY);
+				try (ResultSet resultSet = statement.executeQuery(QUERY)) {
+					logger.log(Level.INFO, "Affichage du resultat");
+					while (resultSet.next()) {
+						logger.log(Level.INFO, "{0}",resultSet.getInt("NUM_ET") + " " +resultSet.getString("NOM_ET") +" "+ resultSet.getString("PRENOM_ET"));
+					}
+				}
 			}
-			// Fermeture de l'instruction (liberation des ressources)
-			stmt.close();
-			System.out.println("\nOk.\n");
+			logger.log(Level.INFO, "Fin de l\'exemple");
 		} catch (SQLException e) {
 			e.printStackTrace();// Arggg!!!
-			System.out.println(e.getMessage() + "\n");
+			logger.log(Level.INFO, e.getMessage());
 		}
 	}
 }
